6d3a3f83dd06c46920fd96a73b4e25a0d3167eb4
Felipe Rodrigues dos Santos <liperodrigues82@gmail.com>
2023-04-03 10:32:28 -0300

    refatorando classe SecurityConfig

--- src/main/java/com/ITA/Agil/demo/config/SecurityConfig.java
+++ src/main/java/com/ITA/Agil/demo/config/SecurityConfig.java
@@ -5,15 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     @Autowired
     @Lazy
@@ -24,28 +25,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder managerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        managerBuilder
                 .userDetailsService(usuarioService)
                 .passwordEncoder(passwordEncoder());
+        return managerBuilder.build();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/api/usuarios/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/usuarios/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/api/usuarios/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/api/usuarios/**").hasRole("ADMIN")
-                // começo habilitação do uso do banco de dados H2
-                .antMatchers("/h2-console/**").permitAll()
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/api/usuarios/**").permitAll()
+                    .antMatchers(HttpMethod.GET, "/api/usuarios/**").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.PUT, "/api/usuarios/**").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.DELETE, "/api/usuarios/**").hasRole("ADMIN")
+                    // começo habilitação do uso do banco de dados H2
+                    .antMatchers("/h2-console/**").permitAll()
                 .and()
-                .headers().frameOptions().sameOrigin()
-                // fim habilitação do uso do banco de dados H2
+                    .headers().frameOptions().sameOrigin()
+                    // fim habilitação do uso do banco de dados H2
                 .and()
                     .httpBasic();
+        return http.build();
     }
 }
